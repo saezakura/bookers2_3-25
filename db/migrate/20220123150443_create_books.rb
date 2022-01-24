64481561af8d1bcbae569c48e2061c95dc8b188c
EC2 Default User <ec2-user@ip-172-31-39-245.ec2.internal>
2022-01-24 07:30:43 +0000

    [Finish]Book_model

--- db/migrate/20220123150443_create_books.rb
+++ db/migrate/20220123150443_create_books.rb
@@ -1,7 +1,9 @@
 class CreateBooks < ActiveRecord::Migration[6.1]
   def change
     create_table :books do |t|
-
+      yut.integer :user_name
+      t.string :text
+      t.text :opinion
       t.timestamps
     end
   end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2022_01_23_140203) do
+ActiveRecord::Schema.define(version: 2022_01_23_150443) do
+
+  create_table "books", force: :cascade do |t|
+    t.integer "user_name"
+    t.string "text"
+    t.text "opinion"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
