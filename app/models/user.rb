c394f41443448c572fdb7c571b995cb5eb86f789
EC2 Default User <ec2-user@ip-172-31-39-245.ec2.internal>
2022-01-27 12:48:23 +0000

    [Add]error

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,12 +9,12 @@ class User < ApplicationRecord
 
   validates :name, presence: true
 
-  def get_profile_image(size)
-   unless profile_image.attached?
-    file_path = Rails.root.join('no_image.jpg')
-    profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpg')
-   end
-   profile_image.variant(resize: size).processed
-  end
 
+   def get_profile_image(size)
+    unless profile_image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      profile_image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    profile_image.variant(resize: "100x100").processed
+   end
 end
