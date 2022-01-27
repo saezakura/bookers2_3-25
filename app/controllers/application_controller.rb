d8c994ad50d2dcb731e26351da28f1279046ac05
EC2 Default User <ec2-user@ip-172-31-39-245.ec2.internal>
2022-01-27 02:45:18 +0000

    [add]index_user_page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,6 +2,10 @@ class ApplicationController < ActionController::Base
   # 指定したデータを保存できるように許可を与える
   before_action :configure_permitted_paramenters, if: :devise_controller?
 
+  def after_sign_in_path_for(resource)
+    books_path
+  end
+
   protected
 
   def configure_permitted_paramenters
--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 Rails.application.routes.draw do
   root to:'homes#top'
+  get 'homes/about' => 'homes#about',as: 'about'
   devise_for :users
   resources :books, only:[:new, :index, :create, :show, :edit, :update, :destroy]
   resources :users,only:[:index, :show, :edit, :update]
